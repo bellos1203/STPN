02533d9e5d26fa66069ccf804345270cd694c768
bellos1203 <bellos1203@snu.ac.kr>
2018-11-06 16:27:14 +0900

    Modify typo

--- code/test.sh
+++ code/test.sh
@@ -2,5 +2,5 @@
 
 # Usage: CUDA_VISIBLE_DEVICES=[gpu_ID] python3 stpn_main.py --ckpt [ckpt_num] --mode test --test_iter [number_of_iteration_tou_want_to_check]
 
-CUDA_VISIBLE_DEVICES=1 python3 stpn_main.py --ckpt 1 --mode test --test_iter 23900
+CUDA_VISIBLE_DEVICES=1 python3 stpn_main.py --ckpt 1 --mode test --test_iter 23100
 
